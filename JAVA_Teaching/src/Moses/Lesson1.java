f0c25b6afc4501d74f72f5a94eeda52f7512eb41
Goutham Kumar Pagadal <pagadalagoutham@gmail.com>
2018-09-13 20:13:21 +0530

    Second Commit

--- JAVA_Teaching/src/Moses/Lesson1.java
+++ JAVA_Teaching/src/Moses/Lesson1.java
@@ -5,20 +5,7 @@ public class Lesson1 {
 	public static void main(String[] args) {
 		
 			 
-			      int a=1;
-			  
-			      switch(a)
-			      {
-			         case 0:
-			             System.out.println("Case 0");
-			             break;
-			         case 1:
-			             System.out.println("Case 1");
-			             break;
-			         default:
-			             System.out.println("Default Case");
-			       }
-			    
+			     
 			 
 		
 
